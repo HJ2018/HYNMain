99e2efb70ada9eb09055b2770149519e7fcc1b86
jie.huang <jie.huang@jiehuangdeMacBook-Pro.local>
2018-04-03 09:47:42 +0800

    ..

---
+++ Example/Pods/Target Support Files/HYNMain/HYNMain-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_HYNMain : NSObject
+@end
+@implementation PodsDummy_HYNMain
+@end
---
+++ Example/Pods/Target Support Files/HYNMain/HYNMain-umbrella.h
@@ -0,0 +1,21 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "XMGNavigationController.h"
+#import "XMGTabBarController.h"
+#import "XMGMiddleView.h"
+#import "XMGNavBar.h"
+#import "XMGTabBar.h"
+
+FOUNDATION_EXPORT double HYNMainVersionNumber;
+FOUNDATION_EXPORT const unsigned char HYNMainVersionString[];
+
